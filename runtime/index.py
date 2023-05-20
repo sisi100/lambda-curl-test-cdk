c3f27dbc41b1b65b7d00c2fa98e4b421348d3a97
Shun Isii <sisii00.swork@gmail.com>
2023-05-20 10:01:05 +0900

    ちょっと追加

--- runtime/index.py
+++ runtime/index.py
@@ -3,12 +3,14 @@ import subprocess
 
 def handler(event, context):
     cmd = subprocess.run(
+        # "curl --version", # curl 7.88.1
         "curl https://blog.i-tale.jp/",
         shell=True,
         capture_output=True,
         text=True,
     )
-    return {
-        "stdout": cmd.stdout,
-        "stderr": cmd.stderr,
-    }
+    print("====> stdout")
+    print(cmd.stdout)
+    print("====> stderr")
+    print(cmd.stderr)
+    return
